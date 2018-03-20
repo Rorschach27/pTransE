1e1994987b3245e29bcc212b5e23c177814045b1
sbachlet <sbachlet@gmail.com>
2018-03-20 10:57:12 -0600

    Initial Commit

--- wiki_data/wiki_read.py
+++ wiki_data/wiki_read.py
@@ -2,10 +2,11 @@ from gensim.corpora import WikiCorpus
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-inp = os.path.join(dir_path, 'enwiki-latest-pages-articles10.xml-p2336425p3046511.bz2')
+inp = os.path.join(dir_path, 'eswiki-20180301-pages-articles1.xml-p5p229076.bz2')
 outp = os.path.join(dir_path, 'wiki2')
 wiki = WikiCorpus(inp, lemmatize=False)
-output = open(outp, 'w')
+output = open(outp,mode='w', encoding='utf_32')
 for text in wiki.get_texts():
-    output.write(b' '.join(text).decode('utf-8') + '\n')
+    outText = ' '.join(text) + '\n'
+    output.write(outText)
 output.close()
